8864375412b2c8d850a240839a81e292bd52720d
jorgepavia <jpavia127@gmail.com>
2026-07-08 23:03:37 -0600

    Portafolio U2 - Estructura

--- file.py
+++ file.py
@@ -1,1 +1,5 @@
-print("hello world")
+año = int(input("Ingresa un año: "))
+if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+      print("Ano bisiesto")
+else:
+      print("No es ano bisiesto")
